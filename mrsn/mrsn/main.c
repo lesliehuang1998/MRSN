092a02061fc99f9851791eeb10b421d7ef7dd69b
wenbo <15760343173@qq.com>
2020-03-24 15:54:58 +0800

    First Commit

---
+++ mrsn/mrsn/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mrsn
+//
+//  Created by 黄若禹 on 2020/3/24.
+//  Copyright © 2020 黄若禹. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
